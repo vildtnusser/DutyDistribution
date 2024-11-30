89490fcef60781bc5b2c154486c2cc27c6c38f90
vildtnusser <anna-rosa@hotmail.dk>
2024-11-30 12:31:14 +0100

    WIP

---
+++ DutyDistribution/Components/Pages/DistributionPageBase.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Components;
+
+
+namespace DutyDistribution.Components.Pages;
+
+public class DistributionPageBase : ComponentBase
+{
+    public int currentCount = 0;
+    public void DistributeDuties()
+    {
+        currentCount++;
+    }
+    public void AddNewDuty()
+    {
+        currentCount++;
+    }
+    
+    public WeatherForecast[]? forecasts;
+
+    protected override async Task OnInitializedAsync()
+    {
+        // Simulate asynchronous loading to demonstrate streaming rendering
+        await Task.Delay(500);
+
+        var startDate = DateOnly.FromDateTime(DateTime.Now);
+        var summaries = new[] { "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" };
+        forecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = startDate.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = summaries[Random.Shared.Next(summaries.Length)]
+        }).ToArray();
+    }
+
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+        public int TemperatureC { get; set; }
+        public string? Summary { get; set; }
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+}
---
+++ DutyDistributionTest/DistributionPageBaseTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using DutyDistribution.Components.Pages;
+
+namespace DutyDistributionTest;
+
+public class DistributionPageBaseTest
+{
+    public DistributionPageBase distributionPageBase;
+    
+    [SetUp]
+    public void Setup()
+    {
+        var _distributionPageBase = new DistributionPageBase();
+        distributionPageBase = _distributionPageBase;
+    }
+
+    [Test]
+    public void currentCountInitsTo0Test()
+    {
+        Assert.That(distributionPageBase.currentCount, Is.EqualTo(0));
+    }
+    
+    
+}
