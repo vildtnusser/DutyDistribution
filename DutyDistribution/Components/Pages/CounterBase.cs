e683f6ec714097e4118cc1092f8eb288ead45f56
vildtnusser <anna-rosa@hotmail.dk>
2024-11-30 11:30:24 +0100

    initializing tests

---
+++ DutyDistribution/Components/Pages/CounterBase.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace DutyDistribution.Components.Pages;
+
+public class CounterBase : ComponentBase
+{
+    public int currentCount = 0;
+
+    public void IncrementCount()
+    {
+        currentCount++;
+    }
+    public void DecrementCount()
+    {
+        currentCount--;
+    }
+    
+}
---
+++ DutyDistributionTest/CounterBaseTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using DutyDistribution.Components.Pages;
+
+
+namespace DutyDistributionTest;
+
+public class Tests
+{
+    public CounterBase counterBase;
+    
+    [SetUp]
+    public void Setup()
+    {
+         var _counterBase = new CounterBase();
+         counterBase = _counterBase;
+    }
+
+    [Test]
+    public void currentCountInitsTo0Test()
+    {
+        Assert.That(counterBase.currentCount, Is.EqualTo(0));
+    }
+    
+    
+}
