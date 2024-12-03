ec1bc3db7177532926e2f6256d34c34472729b9e
vildtnusser <anna-rosa@hotmail.dk>
2024-12-03 10:20:28 +0100

    WIP

--- DutyDistribution/Components/Pages/DistributionPageBase.cs
+++ DutyDistribution/Components/Pages/DistributionPageBase.cs
@@ -1,15 +1,55 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.Logging;
 
+
 namespace DutyDistribution.Components.Pages;
 
 public class DistributionPageBase : ComponentBase
 {
-    public DefaultModal defaultModal;
+    public Boolean show = false;
     public int currentCount = 0;
-    public void DistributeDuties()
+
+    public Tuple<List<string>, List<string>> DistributeDuties(List<string> duties, Boolean show)
     {
-        currentCount++;
+        List<String> personList = new()
+        {
+            "P1",
+            "P2"
+        };
+
+        int N = duties.Count;
+        int M = personList.Count;
+        List<String> person1List = new();
+        List<String> person2List = new();
+
+        foreach (string duty in duties)
+        {
+            int i = duties.IndexOf(duty);
+            if (i % 2 == 0)
+            {
+                person1List.Add(duty);
+            }
+            else
+            {
+                person2List.Add(duty);
+            }
+        }
+
+        if (show)
+        {
+            return Tuple.Create(person1List.Distinct().ToList(), person2List.Distinct().ToList());
+        }
+        else
+        {
+            return Tuple.Create(new List<String>()
+                {
+                    "Try to click distribute button again or check if no duties exists"
+                },
+                new List<String>()
+                {
+                    "Try to click distribute button again or check if no duties exists"
+                });
+        }
     }
     
     
