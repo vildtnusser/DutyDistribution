d6e43b0e9baa5ca7828f9ac5775f34467ac812b1
vildtnusser <anna-rosa@hotmail.dk>
2024-12-03 15:02:08 +0100

    WIP- missing distribution of all duties and missing splitting the main page into subpages

--- DutyDistribution/Components/Pages/DistributionPageBase.cs
+++ DutyDistribution/Components/Pages/DistributionPageBase.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
 
@@ -8,7 +9,28 @@ public class DistributionPageBase : ComponentBase
 {
     public Boolean show = false;
     public int currentCount = 0;
-    
+
+    public List<int> getDistributionIndexes(List<string> persons, List<string> duties, string person)
+    {
+        int N = duties.Count;
+        int M = persons.Count;
+        int amountOfDutiesForEachPerson = Convert.ToInt32(Math.Ceiling(N / (double)M));
+        int j = persons.IndexOf(person);
+        if (j == M - 1)
+        {
+            var indexStart = N - amountOfDutiesForEachPerson;
+            var indexEnd = N - 1;
+            return new List<int>(){indexStart,indexEnd};
+        }
+
+        else
+        { 
+            var indexStart = j * amountOfDutiesForEachPerson; 
+            var indexEnd = (j + 1) * amountOfDutiesForEachPerson;
+          return new List<int>() {indexStart,indexEnd};
+
+        }
+    }
 
     //public Tuple<List<string>, List<string>> DistributeDuties(List<string> duties,List<string> persons, Boolean show)
     
@@ -57,48 +79,25 @@ public class DistributionPageBase : ComponentBase
     public List<List<string>> DistributeDuties(List<string> duties, List<string> persons, Boolean show)
     {
         List<List<string>> dutyDistributions = new List<List<string>>();
-        int N = duties.Count;
-        int M = persons.Count;
-        int amountOfDutiesForEachPerson = Convert.ToInt32(Math.Ceiling(N / (double)M));
-        List<string> randomDuties = duties.OrderBy(_ => Guid.NewGuid()).ToList();
-        Console.WriteLine("Random duties: " + randomDuties.ToList());
+        List<string> randomDuties = duties.OrderBy(_ => Random.Shared.Next()).ToList();
+       
+
 
         foreach (var person in persons)
         {
-            List<string> dutyList = new List<string>(){person};
-            int j = persons.IndexOf(person);
-            if (j == M - 1)
-            {
-                int a = N - amountOfDutiesForEachPerson;
-                int b = N - 1;
-            }
-
-            else
-            {
-                int a = j * amountOfDutiesForEachPerson;
-                int b = (j + 1) * amountOfDutiesForEachPerson;
-
-            }
-            
-            // TODO get the distribution to work by getting the index definitions to work in the inner for loop, as it doesn't distribute correctly now
-
-           /*int IndexStart = a;
-            int IndexEnd = b;*/
-            
+            List<string> dutyList = new List<string>() { person };
+            int indexStart = getDistributionIndexes( persons, duties, person)[0];
+            int indexEnd = getDistributionIndexes( persons, duties, person)[1];
+            Console.WriteLine("start index:" + indexStart + " end index:" + indexEnd + " for person " + person);
 
             foreach (string duty in randomDuties)
+                //TODO ensure all duties gets distributed, at the moment there is a bug such that not all duties get distributed
             {
-                int IndexStart = 0;
-                int IndexEnd = amountOfDutiesForEachPerson;
-                    
                 int i = randomDuties.IndexOf(duty);
-                if (Enumerable.Range(IndexStart, IndexEnd - IndexStart + 1).Contains(i))
+                if (Enumerable.Range(indexStart, indexEnd).Contains(i))
                 {
                     dutyList.Add(duty);
                 }
-
-                IndexStart = IndexEnd;
-                IndexEnd = N-1;
             }
 
             dutyDistributions.Add(dutyList.Distinct().ToList());
@@ -130,6 +129,8 @@ public class DistributionPageBase : ComponentBase
         "Cooking",
         "Cleaning"
     };
+    
+    public string nameTextFieldPerson = "";
     
     public List<string> persons = new()
     {
